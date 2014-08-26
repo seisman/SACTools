86897d360314d420310b6d4aef8875a07e2e3c60
Dongdong Tian <seisman.info@gmail.com>
2014-08-26 22:33:54 +0800

    add -N option to suppress filename output

--- saclh.c
+++ saclh.c
@@ -20,13 +20,17 @@ void usage()
     fprintf(stderr, "List the values of selected head fields                \n");
     fprintf(stderr, "                                                       \n");
     fprintf(stderr, "Usage:                                                 \n");
-    fprintf(stderr, "  saclh -H head_fields_lists sacfiles                  \n");
+    fprintf(stderr, "  saclh -H head_fields_lists [-N] sacfiles             \n");
+    fprintf(stderr, "                                                       \n");
+    fprintf(stderr, "Options:                                               \n");
+    fprintf(stderr, "  -N:  do not output filename in colunm 1              \n");
     fprintf(stderr, "                                                       \n");
     fprintf(stderr, "Note:                                                  \n");
     fprintf(stderr, "  1. lists should be seperated by commas               \n");
     fprintf(stderr, "                                                       \n");
     fprintf(stderr, "Examples:                                              \n");
     fprintf(stderr, "  saclh -H evla,evlo,stla,stlo seis1 seis2             \n");
+    fprintf(stderr, "  saclh -H evla -N seis                                \n");
 }
 
 int main(int argc, char *argv[])
@@ -35,10 +39,11 @@ int main(int argc, char *argv[])
     char *p;
     int head[20];
     int cnt = 0;
+    int noname = 0;
 
     int i;
 
-    while ((c=getopt(argc, argv, "H:h")) != -1) {
+    while ((c=getopt(argc, argv, "H:Nh")) != -1) {
         switch (c) {
             case 'H':
                 p = strtok(optarg, ",/");
@@ -51,6 +56,9 @@ int main(int argc, char *argv[])
                     cnt++;
                     p = strtok(NULL, ",/");
                 }
+                break;
+            case 'N':
+                noname = 1;
                 break;
             case 'h':
                 usage();
@@ -70,7 +78,8 @@ int main(int argc, char *argv[])
 
     for (i=optind; i<argc; i++) {   /* loop over files */
         if ((read_sac_head(argv[i], &hd)) != 0) continue;
-        printf("%s ", argv[i]);
+
+        if (noname==0) printf("%s ", argv[i]);
         for (j=0; j<cnt; j++) {
             if (head[j] < SAC_HEADER_FLOATS) {
                 float *pt = &hd.delta;
